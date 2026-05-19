b02c3fbd2e40d686f7c6fdbc9b16480925880b2d
sofiadoenitz <so.doenitz@duocuc.cl>
2026-05-18 21:05:48 -0400

    kkkk

--- ms-factorizacion/src/main/java/com/example/ms_factorizacion/MsFactorizacionApplication.java
+++ ms-factorizacion/src/main/java/com/example/ms_factorizacion/MsFactorizacionApplication.java
@@ -2,8 +2,10 @@ package com.example.ms_factorizacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsFactorizacionApplication {
 
 	public static void main(String[] args) {
